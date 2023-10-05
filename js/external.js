52d9343681321ede3d877904d84a6bf8900b2e3d
Spencer  Pogue <spencerpogue@Spencers-MBP-2.localdomain>
2023-10-05 14:05:20 -0500

    external js exercise

---
+++ js/external.js
@@ -0,0 +1,54 @@
+// console.log("Hello from external JavaScript");
+
+// alert("Welcome to my Website!");
+//
+// // let favColor = prompt("What is your favorite color?");
+// // alert(`Your favorite color is ${favColor}`);
+// let favColor = prompt("What's your favorite color?");
+// alert(`Your favorite color is ${favColor}?! Mine too!!!`);
+
+// let mermaid = prompt("How many days will you be renting The Little Mermaid?");
+// alert(`You chose to rent The Little Mermaid ${mermaid} days.`)
+// let bear = prompt("How many days will you be renting Brother Bear?")
+// alert(`You chose to rent Brother Bear ${bear} days.`)
+// let hercules = prompt("How many days will you be renting Hercules?");
+// alert(`You chose to rent Hercules ${hercules} days.`)
+// let pricePerMovie = 3
+// let totalMoviePrice = pricePerMovie * (parseFloat(mermaid) + parseFloat(bear) + parseFloat(hercules));
+// let totalMoviePriceS = totalMoviePrice.toLocaleString("en-US", { style: "currency", currency: "USD"});
+// console.log(totalMoviePriceS);
+// alert("Your total movie price is " + totalMoviePriceS);
+// //totalCost = totalCost.toLocaleString("en-US", { style: "currency", currency: "USD"});
+//
+// const mrd = 3
+// const brthbr = 5
+// const hrcls = 1
+//
+// const prprd = 3
+//
+// const mrdCost = mrd * prprd;
+// const brthbrCost = brthbr * prprd;
+// const hrclsCost = hrcls * prprd;
+//
+// let totalCost = mrdCost + brthbrCost + hrclsCost
+// // totalCost = `$${totalCost.toFixed(2)}`;
+// totalCost = totalCost.toLocaleString("en-US", { style: "currency", currency: "USD"});
+// console.log(`The total cost is: ${totalCost}`);
+
+
+
+const Gpay = 400;
+const Apay = 380;
+const Fpay = 350;
+const GHours = prompt("How many google hours did you work?");
+const AHours = prompt("How many Amazon hours did you work?");
+const FHours = prompt("How many Facebook hours did you work?");
+const Google = parseFloat(Gpay * GHours);
+const Amazon = parseFloat(Apay * AHours);
+const Facebook = parseFloat(Fpay * FHours);
+const totalPay = Google + Amazon + Facebook;
+console.log("$" + totalPay);
+alert("Your total pay for the week is:" + totalPay.toLocaleString("en-US", { style: "currency", currency: "USD"}));
+// git add -A or git add .           (a period at the end)
+// git commit -m 'your message here'
+// git push origin main
--- js/js-with-html-lecture.js
+++ js/js-with-html-lecture.js
@@ -53,61 +53,73 @@
 // bankAccount = parseInt(bankAccount);
 // console.log("parseInt ==> " + bankAccount)
 
-let x = "griffin";
-let xNumber = Number(x);
-console.log(xNumber);
-//WILL BE USED ON THE ASSESSMENT
-let isEqual = xNumber === NaN;
-console.log("NaN strict comparison")
-let isReallyEqual = isNaN(xNumber);
-console.log("isNaN function" + isReallyEqual);
+// let x = "griffin";
+// let xNumber = Number(x);
+// console.log(xNumber);
+// //WILL BE USED ON THE ASSESSMENT
+// let isEqual = xNumber === NaN;
+// console.log("NaN strict comparison")
+// let isReallyEqual = isNaN(xNumber);
+// console.log("isNaN function" + isReallyEqual);
+//
+// /// truthy/falsey
+//
+// let emptyString = "" == false;
+// console.log(emptyString)
+// let zero = 0 == false;
+// console.log("zero ==> ", zero);
+// let noValue; //undefined
+// console.log(noValue)
+// let noData = null; //null: the intentional absense of data
+//
+// let middleName = "Adam"
+//
+//
+//
+// if(middleName) {
+//     console.log("YUP, IT'S TRUTHY");
+// } else {
+//     console.log("NOPE, IT'S FALSE");
+// }
+//
+//
+// //STRING METHODS
+// let username = "Justino(Nova)";
+// let guildName;
+//
+// let nameLength = username.length;
+// console.log("nameLength ==> ", nameLength);
+// let guildNameStart = username.indexOf("(");
+// console.log("guildNameStart ==> ", guildNameStart)
+// let guildNameEnd = nameLength - 1;
+// guildName = username.substring(guildNameStart + 1, guildNameEnd);
+// console.log("guildName ==> ", guildName);
+// username = username.replace("(" + guildName + ")", "").trim();
+// console.log("username ==> ", username);
+//
+//
+//
+// //TEMPLATE STRINGS
+// let channelMessage = "username from guildName guild has logged in.";
+// channelMessage = username + " from the " + guildName + " guild has logged in";
+// channelMessage = `${username} from the ${guildName} guild has logged in.`;
+// console.log(channelMessage);
+//
+// let i = 35;
+// console.log(i++);
+// i++;
+// console.log(++i);
 
-/// truthy/falsey
 
-let emptyString = "" == false;
-console.log(emptyString)
-let zero = 0 == false;
-console.log("zero ==> ", zero);
-let noValue; //undefined
-console.log(noValue)
-let noData = null; //null: the intentional absense of data
+// alert("Message goes here.");
+//
+// age = 25
 
-let middleName = "Adam"
+// let answer = confirm("Are you sure you want XYZ");
+// console.log(`The user's answer was ${answer}`);
 
-
-
-if(middleName) {
-    console.log("YUP, IT'S TRUTHY");
-} else {
-    console.log("NOPE, IT'S FALSE");
-}
-
-
-//STRING METHODS
-let username = "Justino(Nova)";
-let guildName;
-
-let nameLength = username.length;
-console.log("nameLength ==> ", nameLength);
-let guildNameStart = username.indexOf("(");
-console.log("guildNameStart ==> ", guildNameStart)
-let guildNameEnd = nameLength - 1;
-guildName = username.substring(guildNameStart + 1, guildNameEnd);
-console.log("guildName ==> ", guildName);
-username = username.replace("(" + guildName + ")", "").trim();
-console.log("username ==> ", username);
-
-
-
-//TEMPLATE STRINGS
-let channelMessage = "username from guildName guild has logged in.";
-channelMessage = username + " from the " + guildName + " guild has logged in";
-channelMessage = `${username} from the ${guildName} guild has logged in.`;
-console.log(channelMessage);
-
-let i = 35;
-console.log(i++);
-i++;
-console.log(++i);
+// //Prompt Function
+// let favColor = prompt("What is your favorite color?");
+// alert(`Your favorite color is ${favColor}`);
 
 
